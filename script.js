d24636fcfaf9a4e7f7caa9a9778c6175f88dbe5d
Disgrazed <thedisgraced1@gmail.com>
2025-08-21 10:56:40 +0000

    Handle divisibility by Zero

--- script.js
+++ script.js
@@ -42,15 +42,23 @@ allClear.addEventListener("click", () => {
 })
 
 showKeyNum.forEach((item) => {
+
+
     item.addEventListener("click", () => {
         if (flag == true) {
             display.textContent = null;
             flag = false;
         }
-        display.textContent = `${display.textContent}${item.textContent}`;
+
+        if (operator == "/" && item.textContent == "0" && display.textContent == "") {
+            alert("Can't divide with zero")
+        }
+        else { display.textContent = `${display.textContent}${item.textContent}` };
 
 
     })
+
+
 })
 
 
@@ -62,8 +70,7 @@ showKeyOp.forEach((item) => {
             flag = false;
         }
 
-        if (!(display.textContent === "")) 
-            {arr.push(display.textContent) };
+        if (!(display.textContent === "")) { arr.push(display.textContent) };
 
         display.textContent = null;
 
@@ -98,8 +105,8 @@ showKeyOp.forEach((item) => {
 
         }
 
-
-        arr[1] = item.textContent;
+        operator = item.textContent;
+        arr[1] = operator;
 
     })
 })
@@ -107,47 +114,46 @@ showKeyOp.forEach((item) => {
 
 equals.addEventListener("click", () => {
     if (flag == true) {
-            display.textContent = null;
-            flag = false;
-        }
-
-    if (!(display.textContent === "")) 
-            {arr.push(display.textContent) };
-
         display.textContent = null;
+        flag = false;
+    }
+
+    if (!(display.textContent === "")) { arr.push(display.textContent) };
+
+    display.textContent = null;
 
     if (arr.length == 3) {
-            arr = arr.map((item) => {
-                if (item == "+" || item == "-" || item == "/" || item == "*") { return item }
-                else { return Number(item) };
-            })
-            let result;
-            switch (arr[1]) {
-                case "+":
-                    result = sum(arr[0], arr[2]);
-                    break;
-                case "-":
-                    result = subtract(arr[0], arr[2]);
-                    break;
-                case "*":
-                    result = multiply(arr[0], arr[2]);
-                    break;
-                case "/":
-                    result = divide(arr[0], arr[2]);
-                    break;
-            }
-
-            arr.length = 0;
-
-
-            arr.push(result);
-            display.textContent = result.toFixed(2);
-            flag = true;
-
+        arr = arr.map((item) => {
+            if (item == "+" || item == "-" || item == "/" || item == "*") { return item }
+            else { return Number(item) };
+        })
+        let result;
+        switch (arr[1]) {
+            case "+":
+                result = sum(arr[0], arr[2]);
+                break;
+            case "-":
+                result = subtract(arr[0], arr[2]);
+                break;
+            case "*":
+                result = multiply(arr[0], arr[2]);
+                break;
+            case "/":
+                result = divide(arr[0], arr[2]);
+                break;
         }
 
+        arr.length = 0;
+
+
+        arr.push(result);
+        display.textContent = result.toFixed(2);
+        flag = true;
+
+    }
+
     else {
-        Alert("Invalid Action");
+        alert("Invalid Action");
     }
 })
 
