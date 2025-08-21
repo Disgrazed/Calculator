c34cf6149f3eb21d60cd95a13a0edb028ddecf2d
Disgrazed <thedisgraced1@gmail.com>
2025-08-21 10:22:36 +0000

    Add Equal to functionality

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ let flag = false;
 const showKeyNum = document.querySelectorAll(".show.num");
 const showKeyOp = document.querySelectorAll(".show.operator");
 
+const equals = document.querySelector(".eq")
 
 const display = document.querySelector("#display")
 
@@ -101,6 +102,53 @@ showKeyOp.forEach((item) => {
         arr[1] = item.textContent;
 
     })
+})
+
+
+equals.addEventListener("click", () => {
+    if (flag == true) {
+            display.textContent = null;
+            flag = false;
+        }
+
+    if (!(display.textContent === "")) 
+            {arr.push(display.textContent) };
+
+        display.textContent = null;
+
+    if (arr.length == 3) {
+            arr = arr.map((item) => {
+                if (item == "+" || item == "-" || item == "/" || item == "*") { return item }
+                else { return Number(item) };
+            })
+            let result;
+            switch (arr[1]) {
+                case "+":
+                    result = sum(arr[0], arr[2]);
+                    break;
+                case "-":
+                    result = subtract(arr[0], arr[2]);
+                    break;
+                case "*":
+                    result = multiply(arr[0], arr[2]);
+                    break;
+                case "/":
+                    result = divide(arr[0], arr[2]);
+                    break;
+            }
+
+            arr.length = 0;
+
+
+            arr.push(result);
+            display.textContent = result.toFixed(2);
+            flag = true;
+
+        }
+
+    else {
+        Alert("Invalid Action");
+    }
 })
 
 
