eb99219cd236592a764ab040c7aa7f4e10540bc2
Disgrazed <thedisgraced1@gmail.com>
2025-08-21 10:09:28 +0000

    Fantastic Part

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-function sum(x,y) {
-    return x+y;
+function sum(x, y) {
+    return x + y;
 }
 
-function subtract(x,y) {
-    return x-y;
+function subtract(x, y) {
+    return x - y;
 }
 
-function multiply(x,y) {
-    return x*y;
+function multiply(x, y) {
+    return x * y;
 }
 
-function divide(x,y) {
-    return x/y;
+function divide(x, y) {
+    return x / y;
 }
 
 console.log(multiply("\n \n \n 4", .7));
 
 let arr = [];
+let span;
 
 let operator;
+
+
+let flag = false;
 
 
 
@@ -31,36 +35,42 @@ const display = document.querySelector("#display")
 
 const allClear = document.querySelector("#allClear");
 
-allClear.addEventListener("click", ()=> {
+allClear.addEventListener("click", () => {
     display.textContent = null;
     arr.length = 0;
 })
 
 showKeyNum.forEach((item) => {
-   item.addEventListener("click" ,() => {
-
-
+    item.addEventListener("click", () => {
+        if (flag == true) {
+            display.textContent = null;
+            flag = false;
+        }
         display.textContent = `${display.textContent}${item.textContent}`;
 
-        
-        }) 
+
+    })
 })
 
 
 showKeyOp.forEach((item) => {
-   item.addEventListener("click" ,() => {
-        
-         
-        if (!(display.textContent === ""))
-            {arr.push(display.textContent)};
+    item.addEventListener("click", () => {
+
+        if (flag == true) {
+            display.textContent = null;
+            flag = false;
+        }
+
+        if (!(display.textContent === "")) 
+            {arr.push(display.textContent) };
 
         display.textContent = null;
-        
+
 
         if (arr.length == 3) {
             arr = arr.map((item) => {
-            if (item == "+" ||item == "-" ||item == "/"||item == "*") {return item}
-            else {return Number(item)};
+                if (item == "+" || item == "-" || item == "/" || item == "*") { return item }
+                else { return Number(item) };
             })
             let result;
             switch (arr[1]) {
@@ -79,16 +89,18 @@ showKeyOp.forEach((item) => {
             }
 
             arr.length = 0;
-            console.log(result);
-            
+
+
             arr.push(result);
+            display.textContent = result;
+            flag = true;
 
         }
 
 
         arr[1] = item.textContent;
-        
-   }) 
+
+    })
 })
 
 
